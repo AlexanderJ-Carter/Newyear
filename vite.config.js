b8207b435a06c6566e379cca49b52692fa20cebd
Alexander James Carter <2253940186@qq.com>
2025-12-24 20:46:01 +0800

    更新

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 使用相对路径，这样无论部署在根目录还是子目录都能正常工作
   resolve: {
     alias: {
       '@': '/src',
